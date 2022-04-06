c68c8d1c6e44fea341df57c1625a6f567e8a9fad
wdssmq <wdssmq@qq.com>
2022-04-06 13:28:20 +0800

    docsify. 云足迹页计数调整；

--- docs/js-plugins/footmarks-count.js
+++ docs/js-plugins/footmarks-count.js
@@ -5,12 +5,12 @@ function footmarks_count(hook, vm) {
     if (!vm.route.file.startsWith('footmarks/')) {
       return content;
     }
-    vm.config.autoHeader = false;
+    // vm.config.autoHeader = false;
     // 换行符统一
     content = content.replace(/\r/g, '').replace(/\n+$/, '');
     // js count \n in string
     const itemsCount = content.trim().split("\n\n").length - 1;
-    content = content.replace(/\n\n/, ` - 「${itemsCount}」\n\n`);
+    content = content.replace(/\n\n/, ` - ${itemsCount}\n\n`);
     return content;
   });
 }
