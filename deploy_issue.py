f8f8dfd606139281c8a9d66d989d80071d0145a3
wdssmq <wdssmq@qq.com>
2023-08-14 18:42:37 +0800

    style. py 代码格式化

--- deploy_issue.py
+++ deploy_issue.py
@@ -1,9 +1,11 @@
-import requests
+""" 构建云足迹 """
 import os
-import json
-import datetime
+
 import sys
 import re
+# import json
+# import datetime
+import requests
 
 # https://github.com/twfb/DeployIssue
 # https://github.com/twfb/DeployIssue/blob/main/deploy_issue.py
@@ -12,10 +14,9 @@ REPO = sys.argv[1]
 TOKEN = sys.argv[2]
 POST_DIR = "docs/footmarks/"
 
-
 HEADERS = {
     "Accept": "application/vnd.github.v3+json",
-    "Authorization": "token {}".format(TOKEN),
+    "Authorization": "token " + TOKEN,
 }
 
 # 读取目录中的 .md 文件，截取不含后缀的文件名作为文章列表；
@@ -25,8 +26,9 @@ all_posts = list(map(lambda x: x.split(".md")[0], os.listdir(POST_DIR)))
 remove_posts = filter(
     lambda x: x["title"] in all_posts,
     requests.get(
-        "https://api.github.com/repos/{}/issues?labels=unpick".format(REPO),
+        f"https://api.github.com/repos/{REPO}/issues?labels=unpick",
         headers=HEADERS,
+        timeout=10,
     ).json(),
 )
 
@@ -34,53 +36,63 @@ remove_posts = filter(
 add_posts = filter(
     lambda x: "unpick" not in str(x["labels"]),
     requests.get(
-        "https://api.github.com/repos/{}/issues?labels=pick".format(REPO),
+        f"https://api.github.com/repos/{REPO}/issues?labels=pick",
         headers=HEADERS,
+        timeout=10,
     ).json(),
 )
 
-def get_comment(item):
-    strRlt = ""
-    if item["comments"] == 0:
-        return strRlt
-    else:
-        add_comments = filter(
-            lambda x: x["author_association"] == "OWNER",
-            requests.get(
-                item["comments_url"],
-                headers=HEADERS,
-            ).json()
-        )
-        for comment in add_comments:
-            strRlt += comment["body"] + "\n\n"
-        return strRlt
-# 获取评论
 
-def update_sidebar(dst_file, insert_info):
+def get_comment(item):
+    """获取评论"""
+    str_rlt = ""
+    if item["comments"] == 0:
+        return str_rlt
+    add_comments = filter(
+        lambda x: x["author_association"] == "OWNER",
+        requests.get(
+            item["comments_url"],
+            headers=HEADERS,
+            timeout=10,
+        ).json(),
+    )
+    for comment in add_comments:
+        str_rlt += comment["body"] + "\n\n"
+    return str_rlt
+
+
+def update_sidebar(dst_file, arg_info):
+    """更新侧边栏"""
     # 替换 <!-- footmarks --> 到 <!-- footmarks end--> 之间的内容
-    insert_info = "<!-- footmarks -->\n" + insert_info + "\n<!-- footmarks end-->"
-    insert_info = insert_info.replace("docs/", "")
+    arg_info = "<!-- footmarks -->\n" + arg_info + "\n<!-- footmarks end-->"
+    arg_info = arg_info.replace("docs/", "")
     # 获取 dst_file 内容
-    with open(dst_file, 'r', encoding='utf-8') as f:
-        dst_file_content = f.read()
-    # print(insert_info)
+    with open(dst_file, "r", encoding="utf-8") as file:
+        dst_file_content = file.read()
+    # print(arg_info)
     new_dst_file_content = re.sub(
-        r'<!-- footmarks -->(.|\n)+<!-- footmarks end-->', insert_info, dst_file_content, 1)
-    with open(dst_file, 'w', encoding='utf-8', newline="\n") as f:
-        f.write(new_dst_file_content)
+        r"<!-- footmarks -->(.|\n)+<!-- footmarks end-->",
+        arg_info,
+        dst_file_content,
+        1,
+    )
+    with open(dst_file, "w", encoding="utf-8", newline="\n") as file:
+        file.write(new_dst_file_content)
     return True
+
+
 # 更新链接到 _sidebar.md
 
 
 # add, update, delete
 count = [0, 0, 0]
 
-insert_info = ""
+INFO = ""
 
 for post in add_posts:
     file_path = POST_DIR + post["title"] + ".md"
-    insert_info += "  * [{}]({})\n".format(post["title"], file_path)
-    with open(file_path, "w") as f:
+    INFO += f"  * [{post['title']}]({file_path})\n"
+    with open(file_path, "w", encoding="utf-8") as f:
         f.write(post["body"])
         f.write("\n\n")
         f.write(get_comment(post))
@@ -90,11 +102,11 @@ for post in add_posts:
     else:
         count[0] += 1
 
-update_sidebar("docs/_sidebar.md", insert_info)
+update_sidebar("docs/_sidebar.md", INFO)
 os.system("git add docs/_sidebar.md >/dev/null 2>&1")
 
 for post in remove_posts:
     os.system("git rm " + POST_DIR + post["title"] + ".md" + " >/dev/null 2>&1")
     count[2] += 1
 
-print("autoupdate \n add {}，update {}，delete {}".format(count[0], count[1], count[2]))
+print(f"autoupdate \n add {count[0]}，update {count[1]}，delete {count[2]}")
